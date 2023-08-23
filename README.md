7d8bcca38c20a76b5f60da60186317952ec8c967
TomaOfficer <Thomas.officer@gmail.com>
2023-08-23 18:35:09 +0000

    Nuke old packages, start fresh, build langchain chatbot

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Set up
-1. Install dependencies, including langchain and openai
+1. Install langchain, openai
 2. Set env var OPENAI_API_KEY
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-import os
-from flask import Flask, render_template, request, redirect, jsonify
-from replit import web
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.chat_models import ChatOpenAI
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-  return render_template('index.html')
+# Initialize the chat model
+chat = ChatOpenAI()
 
 
-if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080)
+# Function to translate English text to French
+def translate_to_french(english_text):
+  messages = [
+      SystemMessage(
+          content=
+          "You are a helpful assistant that translates English to French."),
+      HumanMessage(content=english_text)
+  ]
+  response = chat(messages)
+  return response.content
+
+
+# Main function for user interaction
+def main():
+  print("Welcome to the English to French Translator!")
+  while True:
+    english_text = input("Enter a sentence in English (or 'quit' to exit): ")
+    if english_text.lower() == 'quit':
+      break
+    french_translation = translate_to_french(english_text)
+    print(f"French Translation: {french_translation}")
+
+
+if __name__ == "__main__":
+  main()
