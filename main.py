6e68ca4662140cd4a0914425a4752f7af226142a
TomaOfficer <Thomas.officer@gmail.com>
2023-08-24 15:13:53 +0000

    Something changed

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.chains import ConversationChain
 from langchain.chat_models import ChatOpenAI
+from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
-# Initialize the chat model
+# Initialize the chat model, an instance of the ChatOpenAI class
 chat = ChatOpenAI()
 
 
-# Function to translate English text to French
-def translate_to_french(english_text):
+# Function to define prompt
+def chat_with_ward(user_input):
   messages = [
-      SystemMessage(
-          content=
-          "You are a helpful assistant that translates English to French."),
-      HumanMessage(content=english_text)
+      SystemMessage(content="You are a formal chatbot named Ward."),
+      HumanMessage(content=user_input)
   ]
   response = chat(messages)
   return response.content
@@ -19,13 +18,12 @@ def translate_to_french(english_text):
 
 # Main function for user interaction
 def main():
-  print("Welcome to the English to French Translator!")
+  print("I am Ward. Let's chat.")
   while True:
-    english_text = input("Enter a sentence in English (or 'quit' to exit): ")
-    if english_text.lower() == 'quit':
-      break
-    french_translation = translate_to_french(english_text)
-    print(f"French Translation: {french_translation}")
+    #The input function is used to take input from the user.
+    user_input = input("You: ")
+    ward_response = chat_with_ward(user_input)
+    print(f"Ward: {ward_response}")
 
 
 if __name__ == "__main__":
